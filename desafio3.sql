ff725ff436d49a9eee9e3f8310379730dcc3a459
Arthur Bernardo Messias <turmessias@gmail.com>
2022-02-03 20:57:28 -0300

    Adiciona Query 3

---
+++ desafio3.sql
@@ -0,0 +1,19 @@
+SELECT usuario,
+COUNT(h.cancao_id) AS qtde_musicas_ouvidas,
+	ROUND(SUM(c.duracao_segundos) / 60, 2) AS total_minutos
+FROM SpotifyClone.usuarios AS u
+LEFT JOIN SpotifyClone.historico_de_reproducoes AS h
+ON u.usuario_id = h.usuario_id
+LEFT JOIN SpotifyClone.cancoes AS c
+ON c.cancao_id = h.cancao_id
+GROUP BY usuario
+ORDER BY usuario ASC;SELECT usuario,
+COUNT(h.cancao_id) AS qtde_musicas_ouvidas,
+	ROUND(SUM(c.duracao_segundos) / 60, 2) AS total_minutos
+FROM SpotifyClone.usuarios AS u
+LEFT JOIN SpotifyClone.historico_de_reproducoes AS h
+ON u.usuario_id = h.usuario_id
+LEFT JOIN SpotifyClone.cancoes AS c
+ON c.cancao_id = h.cancao_id
+GROUP BY usuario
+ORDER BY usuario ASC;
